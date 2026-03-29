4362e7c5b537aaa152669d294d8cdab6c03f37cc
nAehT157 <24110157@student.hcmute.edu.vn>
2026-03-30 00:14:09 +0700

    code của MonAn, LoaiMonAn, NhanVien, TaiKhoan

--- src/main/java/ltw/ck/quanlyquanan/model/entity/LoaiMonAn.java
+++ src/main/java/ltw/ck/quanlyquanan/model/entity/LoaiMonAn.java
@@ -1,4 +1,58 @@
 package ltw.ck.quanlyquanan.model.entity;
 
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "loai_mon_an")
 public class LoaiMonAn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long maLoai;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String tenLoai;
+
+    @OneToMany(mappedBy = "loaiMonAn", cascade = CascadeType.ALL)
+    private List<MonAn> monAns = new ArrayList<>();
+
+    public LoaiMonAn() {
+    }
+
+    public LoaiMonAn(String tenLoai) {
+        this.tenLoai = tenLoai;
+    }
+
+    public Long getMaLoai() {
+        return maLoai;
+    }
+
+    public String getTenLoai() {
+        return tenLoai;
+    }
+
+    public List<MonAn> getMonAns() {
+        return monAns;
+    }
+
+    public void setTenLoai(String tenLoai) {
+        this.tenLoai = tenLoai;
+    }
+
+    public void addMonAn(MonAn monAn) {
+        monAns.add(monAn);
+        monAn.setLoaiMonAn(this);
+    }
+
+    public void removeMonAn(MonAn monAn) {
+        monAns.remove(monAn);
+        monAn.setLoaiMonAn(null);
+    }
+
+    @Override
+    public String toString() {
+        return tenLoai;
+    }
 }
--- src/main/java/ltw/ck/quanlyquanan/model/entity/TaiKhoan.java
+++ src/main/java/ltw/ck/quanlyquanan/model/entity/TaiKhoan.java
@@ -1,4 +1,64 @@
 package ltw.ck.quanlyquanan.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tai_khoan")
 public class TaiKhoan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long maTK;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String tenDangNhap;
+
+    @Column(nullable = false)
+    private String matKhau;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ma_nv", unique = true)
+    private NhanVien nhanVien;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(String tenDangNhap, String matKhau, NhanVien nhanVien) {
+        this.tenDangNhap = tenDangNhap;
+        this.matKhau = matKhau;
+        this.nhanVien = nhanVien;
+    }
+
+    public Long getMaTK() {
+        return maTK;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public NhanVien getNhanVien() {
+        return nhanVien;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public void setNhanVien(NhanVien nhanVien) {
+        this.nhanVien = nhanVien;
+    }
+
+    @Override
+    public String toString() {
+        return tenDangNhap;
+    }
 }
